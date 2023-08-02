19941c22b088d37068e84144675f26fcda43a287
PawanPatil19 <pawankp1903@gmail.com>
2023-08-03 04:04:00 +0800

    Fix bugs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,14 @@
-import { redirect, useRouter } from 'next/navigation';
 import Image from 'next/image';
 import React, { useState } from 'react';
-import { AiOutlineEye, AiOutlineLike, AiFillApple, AiOutlineCloseCircle, AiOutlineArrowRight } from 'react-icons/ai';
+import { AiOutlineArrowRight, AiOutlineSearch } from 'react-icons/ai';
 import Link from 'next/link';
-import getCurrentUser from './actions/getCurrentUser';
-import ClientOnly from './components/ClientOnly';
-import EmptyState from './components/EmptyState';
 import getPosts from './actions/getPosts';
-import PostDisplay from './components/PostDisplay';
-import { CircularProgress } from '@mui/material';
 import PostsGrid from './components/PostsGrid';
 
 
 
 export default async function Home() {
   const posts = await getPosts();
-
-    
-
 
   return (
     <div className="bg-white px-5 md:px-0">
@@ -67,7 +58,7 @@ export default async function Home() {
                   <div className="flex w-full">
                       <input type="search" id="search-dropdown" className="p-2.5 w-full h-14 text-sm text-gray-400 bg-gray-100 rounded-lg  focus:outline focus:outline-violet-300" placeholder="Search Notes..." required />
                       <button type="submit" className="text-sm font-medium text-white bg-violet-800 rounded-r-lg border-violet-800 hover:bg-violet-800 focus:ring-4 focus:outline-none focus:ring-violet-800 px-5">
-                          <svg aria-hidden="true" className="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path></svg>
+                          <AiOutlineSearch size={25}/>
                           <span className="sr-only">Search</span>
                       </button>
                   </div>
